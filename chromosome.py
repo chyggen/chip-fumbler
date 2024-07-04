7fa85792a86817fbc7b90ade3f64edbaf9a77be0
Chris <chyggenot@gmail.com>
2024-07-03 17:40:03 -0700

    Agent and chromosome skeleton code

---
+++ chromosome.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from dataclasses import dataclass
+
+@dataclass
+class Chromosome:
+    aggressiveness: float
+    bluff_probability: float
+
+    # Parse Chromosome from file
+    def from_file(file: Path):
+        pass
+
+    # Store the chromosome in a file
+    def to_file(self, destination: Path):
+        pass
+
+    # Generate a random chromosome
+    def random():
+        pass

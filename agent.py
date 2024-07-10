d0180da1937ee4be82fefcdb3859ecada2687729
Chris <chyggenot@gmail.com>
2024-07-09 21:00:24 -0700

    simulation_1v1 implementation

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ from typing import Tuple, Optional
 
 from texasholdem.game.game import TexasHoldEm
 from texasholdem.game.action_type import ActionType
+from texasholdem.agents.basic import random_agent
 
 from chromosome import Chromosome
 
@@ -14,8 +15,13 @@ class Agent:
 
     # Decide the next move based on the game state and the agent's chromosome
     # Returns the action to take, plus a number of chips if the action is a raise
-    def action(self, game: TexasHoldEm) -> Tuple[ActionType, Optional[int]]:
-        pass
+    def action(
+        self, game: TexasHoldEm, random: bool = False
+    ) -> Tuple[ActionType, Optional[int]]:
+        if random:
+            return random_agent(game)
+        else:
+            pass
 
     # Get the agent's underlying chromosome
     def get_chromosome(self) -> Chromosome:
--- simulation.py
+++ simulation.py
@@ -5,19 +5,47 @@ from texasholdem.game.game import TexasHoldEm
 from agent import Agent
 
 
-def simulation_1v1(A1: Agent, A2: Agent) -> Tuple[bool, float]:
+# define some constants for setting up the game:
+BUY_IN = 500
+BIG_BLIND = 5
+SMALL_BLIND = 0
+MAX_PLAYERS = 2
+
+
+def simulation_1v1(A0: Agent, A1: Agent, game_log: bool = False) -> Tuple[int, float]:
     """Runs a 1v1 simulation between two agents
 
     Args:
         A1: Agent 1
         A2: Agent 2
+        game_log: if true, generate a game log
 
     Returns:
-        A tuple containing: A bool indicating if A1 won, then a float indicating the
+        A tuple containing: the winner, then a float indicating the
         winner's average chips gained / hand
 
     """
-    pass
+    game = TexasHoldEm(
+        buyin=BUY_IN,
+        big_blind=BIG_BLIND,
+        small_blind=SMALL_BLIND,
+        max_players=MAX_PLAYERS,
+    )
+    while game.is_game_running():
+        game.start_hand()
+        while game.is_hand_running():
+            if game.current_player == 0:
+                game.take_action(*A0.action(game, random=True))
+            else:
+                game.take_action(*A1.action(game, random=True))
+        if game_log:
+            game.export_history("./pgns")  # save history
+
+    # When we arrive here, the game is over.
+    # We can determine who won by checking the active players in the pot:
+    winner = list(game.in_pot_iter(game.btn_loc + 1))[0]
+    chips_per_hand = BUY_IN / game.num_hands
+    return(winner, chips_per_hand)
 
 
 def round_robin(agents: list[Agent]) -> dict[Agent, float]:
@@ -29,4 +57,3 @@ def round_robin(agents: list[Agent]) -> dict[Agent, float]:
     Returns:
         a dict containing a score for each agent.
     """
-    pass

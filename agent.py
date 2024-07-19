7554b9ffb3534e30d827d0a76f8844fc4c8f094e
Kaden Taylor <C0477280@intra.camosun.bc.ca>
2024-07-19 14:56:25 -0700

    started new agent logic

--- agent.py
+++ agent.py
@@ -3,7 +3,8 @@ from typing import Tuple, Optional
 from texasholdem.game.game import TexasHoldEm
 from texasholdem.game.action_type import ActionType
 from texasholdem.agents.basic import random_agent
-
+from texasholdem.game.hand_phase import HandPhase
+from texasholdem.game.action_type import ActionType
 from chromosome import Chromosome
 
 
@@ -16,7 +17,63 @@ class Agent:
     # Decide the next move based on the game state and the agent's chromosome
     # Returns the action to take, plus a number of chips if the action is a raise
     def action(self, game: TexasHoldEm) -> Tuple[ActionType, Optional[int]]:
-        pass
+        x1 = 40#from trained bot persentage for flop
+        x2 = 50#for per flop
+        x3 = 50#for perhand
+        x4 = 50#for river
+        y1 = 60#perobablity of wining for flop
+        y2 = 60#for per flop
+        y3 = 60#for perhand
+        y4 = 60#for river
+        raiseAction = 0
+        potSize = 5000
+        chipSize = 1000
+        
+        #game state 
+        if game.hand_phase == HandPhase.FLOP:
+            if x1 < y1 <= 75 :
+                #do math based on pot size and persetage to call
+               return  'CHECK'
+            if 75 < y1 <= 100 :
+                #do math based on pot size 
+                raiseAction = potSize/chipSize*2 # temp will need to be changed 
+                return 'RAISE {raiseAction}'
+            else:
+                return 'FOLD' 
+            
+        if game.hand_phase == HandPhase.PREFLOP:
+            #pull matts value 
+            if x2 < y2 <= 75 :
+                #do math based on pot size and persetage to call
+               return  call/check
+            if 75 < y2 <= 100 :
+                #do math based on pot size 
+                raiseAction = potSize/chipSize*2 # temp will need to be changed 
+                return 'RAISE {raiseAction}'
+            else:
+                return 'FOLD' 
+               
+        if game.hand_phase == HandPhase.PREHAND:
+            if x3 < y3 <= 75 :
+                #do math based on pot size and persetage to call
+               return  call/check
+            if 75 < y3 <= 100 :
+                #do math based on pot size 
+                raiseAction = potSize/chipSize*2 # temp will need to be changed 
+                return 'RAISE {raiseAction}'
+            else:
+                return 'FOLD' 
+            
+        if game.hand_phase == HandPhase.RIVER:
+            if x4 < y4 <= 75 :
+                #do math based on pot size and persetage to call
+               return  call/check
+            if 75 < y4 <= 100 :
+                #do math based on pot size 
+                raiseAction = potSize/chipSize*2 # temp will need to be changed 
+                return 'RAISE {raiseAction}'
+            else:
+                return 'FOLD'           
 
     # Get the agent's underlying chromosome
     def get_chromosome(self) -> Chromosome:
